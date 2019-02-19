a0667329b1597cac9bb3a548772fc6b12f2ee36d
Paul Herron <pol.herron@gmail.com>
2019-02-19 14:05:39 +0000

    Add project files.

---
+++ SeleniumMain/Program.cs
@@ -0,0 +1,34 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+
+namespace SeleniumMain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string webURL = @"file:///C:/Users/Paul/Source/Repos/Selenium/SeleniumMain/Default.html";
+            IWebDriver driver = new ChromeDriver(@"C:\libraries\");
+            string googleUrl = "https://www.google.com/";
+
+            //ClickPluralsightImageExample.DoSearch(driver, googleUrl);
+            HTMLExample.SelectRadioButtons(driver, webURL);
+            HTMLExample.SelectOptionDDL(driver, webURL);
+            HTMLExample.SelectOptionDDLUsingSupport(driver, webURL);
+            HTMLExample.SelectTableCell(driver, webURL);
+            HTMLExample.SelectTableCellByXPath(driver, webURL);
+            HTMLExample.SelectTableCellByLinkText(driver, webURL);
+            //PluralsightTestWithExpicitWait(driver, webURL);*/
+
+            Console.ReadLine();
+
+            driver.Close();
+
+        }
+
+     
+        }
+
+    }
